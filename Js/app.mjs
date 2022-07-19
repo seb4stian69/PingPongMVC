7773d9f89128b108bb83161f982a5115c06b49e9
seb4stian69 <sebastiandasanhe_1287@hotmail.com>
2022-07-19 13:35:23 -0500

    Se visualizan los elementos

--- Js/app.mjs
+++ Js/app.mjs
@@ -1,59 +1,98 @@
-// Variables globales
-let widthGlobal
-let heightGlobal
-let playingGlobal = false
-let gameOverGlobal = false
-let bars = []
-let ball = null
-const canvas = document.getElementById("canvas")
-let contexto = canvas.getContext('2d')
+
+// Importamos las variables de "entorno"
+import { width,height,canvas,ctx } from "./env.mjs"
+
+// Objeto canvas
+canvas.width = width
+canvas.height = height
 
 
-// Funcion inicial
-class board {
+/*Elementos globales*/
+let player = new HumanPlayer() // Jugador
+let computer = new ComputerPlayer() // CPU
+let ball = new Ball( (width/2)+10 ,  height/2 ) // Ball
 
-    #widthBoard
-    #heightBoard
+/*Global - Views*/
 
-    constructor(height,width){
-        
-        this.heightBoard = height
-        this.widthBoard = width
+let viewsElements = () =>  {
 
-    }
-
-    getWidth(){
-        return this.widthBoard
-    }
-
-    getheight(){
-        return this.heightBoard
-    }
+    // Se actualiza el elemento mostrandolo desde 0 
+    ctx.clearRect(0, 0, width, height)
+    // Se muestra el elemento [Bars] player
+    player.viewsElements()
+    // Se muestra el elemento [Bars] cpu
+    computer.viewsElements()
+    // Se muestra el elemento [Ball]
+    ball.viewsElements()
 
 }
 
-let objectBoard = null // Objeto global
+let fillData = () =>  {
+    player.fillData()
+    computer.fillData(ball)
+    ball.fillData(player.bars, computer.bars)
+}
 
-// Retornar elementos
-const getElements = () =>{
+/* Instancia y creacion de elementos */ 
 
-    let elements = bars
-    elements.push(ball)
-    return elements
+function HumanPlayer() {
+    this.bars = new Bars(width / 2 - 30, height - 20, 80, 20)
+}
+function ComputerPlayer() {
+    this.bars = new Bars(width / 2 - 30, 0, 80, 20)
+}
+HumanPlayer.prototype.viewsElements = () => {
+    this.bars.viewsElements()
+}
+ComputerPlayer.prototype.viewsElements = () =>  {
+    this.bars.viewsElements()
+}
+function Ball(x, y) {
+
+    this.x = x
+    this.y = y
+    this.horizontalSpeed = 0
+    this.verticalSpeed = 3
+    this.radius = 6
+
+}
+Ball.prototype.viewsElements = () =>  {
+
+    ctx.beginPath()
+    ctx.fillStyle = "#111"
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
+
+}
+function Bars(x, y, width, height) {
+
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.horizontalSpeed = 0
+    this.verticalSpeed = 0
+
+}
+Bars.prototype.viewsElements = () =>  {
+
+    ctx.beginPath()
+    ctx.fillStyle = "#111"
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.closePath()
 
 }
 
-// Pintar tablero
-const seeBoard = (height, width) =>{
+/* Funcionalidades de elementos e interacciones */
 
-    objectBoard = new board(height,width) // Objeto global
-    canvas.width = objectBoard.getWidth()
-    canvas.height  = objectBoard.getheight()
+// Eject
+function Eject() {
 
-}
+    requestAnimationFrame(Eject)
+    viewsElements()
+    fillData()
+
+}; Eject()
 
 
-// Funcion principal que lanza la aplicacio
-const eject = () => {
-    seeBoard(300,600)
-}; eject()
---
+++ Js/env.mjs
@@ -0,0 +1,4 @@
+export const width = 800
+export const height = 500
+export const canvas = document.getElementById('canvas')
+export const ctx = canvas.getContext('2d')
